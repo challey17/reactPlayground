c4e412bc5be524077c4fdd214e896497b3e51217
Chayce Knaub <chknaub@yahoo.com>
2020-03-11 20:21:13 -0400

    stopping point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Split from "./composition/Split";
+import "./App.css";
 
 function App() {
-  return <main className="App">{/* content goes here */}</main>;
+  return (
+    <main className="App">
+      <Split />
+      <Split />
+      <Split />
+      <Split />
+    </main>
+  );
 }
 
 export default App;
